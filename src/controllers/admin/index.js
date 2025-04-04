661ab431da6e35a8cd558fc14347d1c828246efe
uzhojiakbar <uzhojiakbar3@gmail.com>
2025-04-04 05:23:48 +0500

    users filter

--- src/controllers/admin/index.js
+++ src/controllers/admin/index.js
@@ -2,11 +2,39 @@
 const { db } = require("../../db/db");
 
 const getAllUsers = (req, res) => {
-  const query = `SELECT id, username, firstname, lastname, group_name, role FROM users`;
+  const { firstname, lastname, username } = req.query;
 
-  db.all(query, [], (err, rows) => {
+  let baseQuery = `SELECT id, username, firstname, lastname, group_name, role FROM users`;
+  const conditions = [];
+  const params = [];
+
+  console.log("Query params:", req.query);
+
+  if (firstname) {
+    conditions.push("firstname LIKE ?");
+    params.push(`%${firstname}%`);
+  }
+  if (lastname) {
+    conditions.push("lastname LIKE ?");
+    params.push(`%${lastname}%`);
+  }
+  if (username) {
+    conditions.push("username LIKE ?");
+    params.push(`%${username}%`);
+  }
+
+  if (conditions.length) {
+    baseQuery += ` WHERE ` + conditions.join(" AND ");
+  }
+
+  db.all(baseQuery, params, (err, rows) => {
     if (err) return res.status(500).json({ error: "Server xatoligi" });
-    res.status(200).json({ users: rows });
+    res.status(200).json({
+      users: rows,
+      usersCount: rows.length,
+      adminsCount: rows.filter((u) => u.role === "admin").length,
+      teachersCount: rows.filter((u) => u.role === "teacher").length,
+    });
   });
 };
 
